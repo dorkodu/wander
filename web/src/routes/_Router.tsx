f6e797b614f21c43dfb4ceb2efb9a09feb18e99a
doruk eray <doruk@dorkodu.com>
2022-12-28 16:02:16 +0300

    (wip) router

--- web/src/routes/_Router.tsx
+++ web/src/routes/_Router.tsx
@@ -27,11 +27,11 @@ function Router() {
 
           {/* Routes that require authentication */}
           <Route path="/home" element={<Home />}>
-            <Route path="/" element={<Feed />} />
-            <Route path="/explore" element={<Explore />} />
-            <Route path="/inbox" element={<Inbox />} />
-            <Route path="/garden" element={<Garden />} />
-            <Route path="/profile" element={<Profile />} />
+            <Route path="/home/feed" element={<Feed />} />
+            <Route path="/home/explore" element={<Explore />} />
+            <Route path="/home/inbox" element={<Inbox />} />
+            <Route path="/home/garden" element={<Garden />} />
+            <Route path="/home/profile" element={<Profile />} />
           </Route>
 
           {/* Error routes & catch all */}
