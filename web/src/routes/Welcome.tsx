17f26977c2fbb9db78e1cd0b33b419978ca89b1f
doruk eray <doruk@dorkodu.com>
2022-12-28 15:37:27 +0300

    update Welcome page with dummy content

--- web/src/routes/Welcome.tsx
+++ web/src/routes/Welcome.tsx
@@ -1,7 +1,22 @@
-function Welcome() {
-  return (
-    <>welcome</>
-  )
-}
+import { FunctionComponent } from "react";
 
-export default Welcome
+const Welcome: FunctionComponent<{}> = ({}) => {
+  return (
+    <div>
+      <h1>Welcome to Wander</h1>
+      <h3>Your digital mind garden.</h3>
+      <p>social & gamified</p>
+      <div>
+        <button>login</button>
+        <button>signup</button>
+      </div>
+      <div>
+        <p>
+          a <b>dorkodu</b> masterpiece.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
