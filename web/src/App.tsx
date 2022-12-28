c8e0e8df653158f609c27af5299f1d86798cc889
doruk eray <doruk@dorkodu.com>
2022-12-28 15:37:41 +0300

    decouple App from its layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,22 @@
 import { Suspense } from "react";
-import { Outlet } from "react-router-dom"
-import { useAppStore } from "./stores/appStore"
+import { Outlet } from "react-router-dom";
+import { Loader, MantineProvider } from "@mantine/core";
+
+import { useAppStore } from "./stores/appStore";
+import theme from "./styles/theme";
 
 function App() {
-  const loading = useAppStore(state => state.getLoading());
-  
+  const loading = useAppStore((state) => state.getLoading());
+
   return (
     <>
-      <Suspense >
-        {loading ? <>loading...</> : <Outlet />}
-      </Suspense>
+      <MantineProvider theme={theme}>
+        <Suspense>
+          {loading ? <Loader color="green" variant="dots" /> : <Outlet />}
+        </Suspense>
+      </MantineProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
