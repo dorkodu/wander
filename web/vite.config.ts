77386de1c4458f77d6eed2e55af5c97d10a2dd6c
doruk eray <doruk@dorkodu.com>
2022-12-28 19:11:42 +0300

    add new aliases to typescript and vite

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
-import path from 'path'
+import path from "path";
 
-import react from '@vitejs/plugin-react';
-import viteCompression from 'vite-plugin-compression';
-import { VitePWA } from 'vite-plugin-pwa';
-import { createHtmlPlugin as html } from 'vite-plugin-html';
+import react from "@vitejs/plugin-react";
+import viteCompression from "vite-plugin-compression";
+import { VitePWA } from "vite-plugin-pwa";
+import { createHtmlPlugin as html } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({
-      jsxRuntime: 'automatic',
-      jsxImportSource: '@emotion/react',
+      jsxRuntime: "automatic",
+      jsxImportSource: "@emotion/react",
       babel: {
-        plugins: ['@emotion/babel-plugin'],
+        plugins: ["@emotion/babel-plugin"],
       },
     }),
     html({ minify: true }),
@@ -25,11 +25,11 @@ export default defineConfig({
       registerType: "autoUpdate",
       injectRegister: "inline",
       workbox: {
-        globPatterns: ["**/*.{html,css,js,ico}"]
+        globPatterns: ["**/*.{html,css,js,ico}"],
       },
       base: "/",
-      manifest: {}
-    })
+      manifest: {},
+    }),
   ],
   server: {
     watch: { usePolling: true },
@@ -37,16 +37,21 @@ export default defineConfig({
     port: 8000,
     strictPort: true,
     proxy: {
-      "/api": "http://wander_api:8001"
-    }
+      "/api": "http://wander_api:8001",
+    },
   },
   resolve: {
     alias: {
-      "@shared": path.resolve(__dirname, '../shared/src'),
-    }
+      "@shared": path.resolve(__dirname, "../shared/src"),
+      "@images": path.resolve(__dirname, "./public/images"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+    },
   },
   build: {
     reportCompressedSize: false,
   },
-  base: process.env.NODE_ENV === "production" ? "https://cdn.dorkodu.com/wander/" : ""
-})
+  base:
+    process.env.NODE_ENV === "production"
+      ? "https://cdn.dorkodu.com/wander/"
+      : "",
+});
