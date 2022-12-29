bf191580b9d6598d1c075ea296b06e31ca9c5b15
doruk eray <doruk@dorkodu.com>
2022-12-29 20:54:42 +0300

    add assets to /web

---
+++ web/src/components/ColorSchemeToggle.tsx
@@ -0,0 +1,29 @@
+import { useMantineColorScheme, ActionIcon } from "@mantine/core";
+import { IconSun, IconMoonStars } from "@tabler/icons";
+
+export function ColorSchemeToggle() {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+
+  return (
+    <ActionIcon
+      onClick={() => toggleColorScheme()}
+      size="lg"
+      sx={(theme) => ({
+        backgroundColor:
+          theme.colorScheme === "dark"
+            ? theme.colors.dark[6]
+            : theme.colors.gray[0],
+        color:
+          theme.colorScheme === "dark"
+            ? theme.colors.yellow[4]
+            : theme.colors.blue[6],
+      })}
+    >
+      {colorScheme === "dark" ? (
+        <IconSun size={18} strokeWidth={2.5} />
+      ) : (
+        <IconMoonStars size={18} strokeWidth={2.5} />
+      )}
+    </ActionIcon>
+  );
+}
--- web/src/routes/Welcome.tsx
+++ web/src/routes/Welcome.tsx
@@ -1,19 +1,47 @@
+import { Loader } from "@mantine/core";
 import { FunctionComponent } from "react";
+import { Title, Image } from "@mantine/core";
+import WanderLogo from "@assets/wander_app.svg";
 
 const Welcome: FunctionComponent<{}> = ({}) => {
   return (
-    <div>
-      <h1>Welcome to Wander</h1>
-      <h3>Your digital mind garden.</h3>
-      <p>social & gamified</p>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <div>
-        <button>login</button>
-        <button>signup</button>
-      </div>
-      <div>
-        <p>
-          a <b>dorkodu</b> masterpiece.
-        </p>
+        <div style={{ textAlign: "center" }}>
+          <Image
+            radius="xs"
+            src={WanderLogo}
+            width={240}
+            fit="contain"
+            withPlaceholder
+            alt="Wander Logo"
+            style={{
+              margin: "1rem auto",
+              paddingRight: ".5rem",
+            }}
+          />
+        </div>
+        <Title order={1} align="center">
+          Welcome to your digital mind garden.
+        </Title>
+        <Title order={3} align="center">
+          social, gamified, brilliant.
+        </Title>
+        <div>
+          <button>login</button>
+          <button>signup</button>
+        </div>
+        <div>
+          <p>
+            a <b>dorkodu</b> masterpiece.
+          </p>
+        </div>
       </div>
     </div>
   );
