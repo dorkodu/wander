7721af507e196cf88a9bbc45b1000573d491335e
doruk eray <doruk@dorkodu.com>
2023-01-02 00:08:48 +0300

    create component: FAQ

---
+++ www/src/components/FAQ.tsx
@@ -0,0 +1,44 @@
+import { Container, Title, Accordion, createStyles } from "@mantine/core";
+
+const useStyles = createStyles((theme) => ({
+  wrapper: {
+    paddingTop: theme.spacing.xl * 2,
+    paddingBottom: theme.spacing.xl * 2,
+    minHeight: 650,
+  },
+
+  title: {
+    marginBottom: theme.spacing.xl * 1.5,
+  },
+
+  item: {
+    borderRadius: theme.radius.md,
+    marginBottom: theme.spacing.lg,
+
+    border: `1px solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[3]
+    }`,
+  },
+}));
+
+const placeholder =
+  "It can’t help but hear a pin drop from over half a mile away, so it lives deep in the mountains where there aren’t many people or Pokémon.It was born from sludge on the ocean floor. In a sterile environment, the germs within its body can’t multiply, and it dies.It has no eyeballs, so it can’t see. It checks its surroundings via the ultrasonic waves it emits from its mouth.";
+
+export function Faq() {
+  const { classes: style } = useStyles();
+
+  return (
+    <Container size="sm" className={style.wrapper}>
+      <Title align="center" className={style.title}>
+        Frequently Asked Questions
+      </Title>
+
+      <Accordion variant="separated">
+        <Accordion.Item className={style.item} value="reset-password">
+          <Accordion.Control>How can I reset my password?</Accordion.Control>
+          <Accordion.Panel>{placeholder}</Accordion.Panel>
+        </Accordion.Item>
+      </Accordion>
+    </Container>
+  );
+}
