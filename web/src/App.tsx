883c16dac9bf21978734b3ddb5dd52e50a4d353e
doruk eray <doruk@dorkodu.com>
2023-01-04 23:43:36 +0300

    [web] add color scheming to app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,15 +7,21 @@ import theme from "./styles/theme";
 
 function App() {
   const loading = useAppStore((state) => state.getLoading());
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
 
   return (
-    <>
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
       <MantineProvider theme={theme}>
         <Suspense>
           {loading ? <Loader color="green" variant="dots" /> : <Outlet />}
         </Suspense>
       </MantineProvider>
-    </>
+    </ColorSchemeProvider>
   );
 }
 
