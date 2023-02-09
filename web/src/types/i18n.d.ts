81c3d9cb1ad8bc115ef1230948438378645404ec
doruk eray <doruk@dorkodu.com>
2023-02-09 21:54:57 +0300

    Create i18n.d.ts

---
+++ web/src/types/i18n.d.ts
@@ -0,0 +1,11 @@
+import "i18next";
+import common from "@assets/locales/en/common.json";
+
+declare module "i18next" {
+  interface CustomTypeOptions {
+    defaultNS: "common";
+    resources: {
+      common: typeof common;
+    };
+  }
+}
