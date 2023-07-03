9ed7deb5fd53cbd7195717123d4e11bbbae06643
doruk eray <doruk@dorkodu.com>
2023-07-04 00:33:07 +0300

    remove example "peer"

--- example/peer/src/components/loaders/DefaultLoader.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader } from "@mantine/core";
-import { useDelay } from "../hooks";
-
-export default function DefaultLoader() {
-  const delay = useDelay();
-  if (delay) return null;
-
-  return <Loader variant="dots" color="green" />
-}
--- example/peer/src/routes/Welcome.tsx
+++
@@ -1,5 +0,0 @@
-export default function Welcome() {
-  return (
-    <>welcome</>
-  )
-}
