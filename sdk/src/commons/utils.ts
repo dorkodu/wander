1e133e4c34d7009997e0711729dc3c8ee9564253
doruk eray <doruk@dorkodu.com>
2023-07-14 20:40:29 +0300

    add nostr's utils to commons

--- sdk/src/commons/utils.ts
+++ sdk/src/commons/utils.ts
@@ -1,20 +1,20 @@
-import type {Event} from './event.ts'
+import type { Event } from "./event";
 
-export const utf8Decoder = new TextDecoder('utf-8')
-export const utf8Encoder = new TextEncoder()
+export const utf8Decoder = new TextDecoder("utf-8");
+export const utf8Encoder = new TextEncoder();
 
 export function normalizeURL(url: string): string {
-  let p = new URL(url)
-  p.pathname = p.pathname.replace(/\/+/g, '/')
-  if (p.pathname.endsWith('/')) p.pathname = p.pathname.slice(0, -1)
+  let p = new URL(url);
+  p.pathname = p.pathname.replace(/\/+/g, "/");
+  if (p.pathname.endsWith("/")) p.pathname = p.pathname.slice(0, -1);
   if (
-    (p.port === '80' && p.protocol === 'ws:') ||
-    (p.port === '443' && p.protocol === 'wss:')
+    (p.port === "80" && p.protocol === "ws:") ||
+    (p.port === "443" && p.protocol === "wss:")
   )
-    p.port = ''
-  p.searchParams.sort()
-  p.hash = ''
-  return p.toString()
+    p.port = "";
+  p.searchParams.sort();
+  p.hash = "";
+  return p.toString();
 }
 
 //
@@ -24,32 +24,32 @@ export function insertEventIntoDescendingList(
   sortedArray: Event<number>[],
   event: Event<number>
 ) {
-  let start = 0
-  let end = sortedArray.length - 1
-  let midPoint
-  let position = start
+  let start = 0;
+  let end = sortedArray.length - 1;
+  let midPoint;
+  let position = start;
 
   if (end < 0) {
-    position = 0
+    position = 0;
   } else if (event.created_at < sortedArray[end].created_at) {
-    position = end + 1
+    position = end + 1;
   } else if (event.created_at >= sortedArray[start].created_at) {
-    position = start
+    position = start;
   } else
     while (true) {
       if (end <= start + 1) {
-        position = end
-        break
+        position = end;
+        break;
       }
-      midPoint = Math.floor(start + (end - start) / 2)
+      midPoint = Math.floor(start + (end - start) / 2);
       if (sortedArray[midPoint].created_at > event.created_at) {
-        start = midPoint
+        start = midPoint;
       } else if (sortedArray[midPoint].created_at < event.created_at) {
-        end = midPoint
+        end = midPoint;
       } else {
         // aMidPoint === num
-        position = midPoint
-        break
+        position = midPoint;
+        break;
       }
     }
 
@@ -58,43 +58,43 @@ export function insertEventIntoDescendingList(
     return [
       ...sortedArray.slice(0, position),
       event,
-      ...sortedArray.slice(position)
-    ]
+      ...sortedArray.slice(position),
+    ];
   }
 
-  return sortedArray
+  return sortedArray;
 }
 
 export function insertEventIntoAscendingList(
   sortedArray: Event<number>[],
   event: Event<number>
 ) {
-  let start = 0
-  let end = sortedArray.length - 1
-  let midPoint
-  let position = start
+  let start = 0;
+  let end = sortedArray.length - 1;
+  let midPoint;
+  let position = start;
 
   if (end < 0) {
-    position = 0
+    position = 0;
   } else if (event.created_at > sortedArray[end].created_at) {
-    position = end + 1
+    position = end + 1;
   } else if (event.created_at <= sortedArray[start].created_at) {
-    position = start
+    position = start;
   } else
     while (true) {
       if (end <= start + 1) {
-        position = end
-        break
+        position = end;
+        break;
       }
-      midPoint = Math.floor(start + (end - start) / 2)
+      midPoint = Math.floor(start + (end - start) / 2);
       if (sortedArray[midPoint].created_at < event.created_at) {
-        start = midPoint
+        start = midPoint;
       } else if (sortedArray[midPoint].created_at > event.created_at) {
-        end = midPoint
+        end = midPoint;
       } else {
         // aMidPoint === num
-        position = midPoint
-        break
+        position = midPoint;
+        break;
       }
     }
 
@@ -103,85 +103,85 @@ export function insertEventIntoAscendingList(
     return [
       ...sortedArray.slice(0, position),
       event,
-      ...sortedArray.slice(position)
-    ]
+      ...sortedArray.slice(position),
+    ];
   }
 
-  return sortedArray
+  return sortedArray;
 }
 
 export class MessageNode {
-  private _value: string
-  private _next: MessageNode | null
+  private _value: string;
+  private _next: MessageNode | null;
 
   public get value(): string {
-    return this._value
+    return this._value;
   }
   public set value(message: string) {
-    this._value = message
+    this._value = message;
   }
   public get next(): MessageNode | null {
-    return this._next
+    return this._next;
   }
   public set next(node: MessageNode | null) {
-    this._next = node
+    this._next = node;
   }
 
   constructor(message: string) {
-    this._value = message
-    this._next = null
+    this._value = message;
+    this._next = null;
   }
 }
 
 export class MessageQueue {
-  private _first: MessageNode | null
-  private _last: MessageNode | null
+  private _first: MessageNode | null;
+  private _last: MessageNode | null;
 
   public get first(): MessageNode | null {
-    return this._first
+    return this._first;
   }
   public set first(messageNode: MessageNode | null) {
-    this._first = messageNode
+    this._first = messageNode;
   }
   public get last(): MessageNode | null {
-    return this._last
+    return this._last;
   }
   public set last(messageNode: MessageNode | null) {
-    this._last = messageNode
+    this._last = messageNode;
   }
-  private _size: number
+  private _size: number;
   public get size(): number {
-    return this._size
+    return this._size;
   }
   public set size(v: number) {
-    this._size = v
+    this._size = v;
   }
 
   constructor() {
-    this._first = null
-    this._last = null
-    this._size = 0
+    this._first = null;
+    this._last = null;
+    this._size = 0;
   }
   enqueue(message: string): boolean {
-    const newNode = new MessageNode(message)
+    const newNode = new MessageNode(message);
     if (this._size === 0 || !this._last) {
-      this._first = newNode
-      this._last = newNode
+      this._first = newNode;
+      this._last = newNode;
     } else {
-      this._last.next = newNode
-      this._last = newNode
+      this._last.next = newNode;
+      this._last = newNode;
     }
-    this._size++
-    return true
+    this._size++;
+    return true;
   }
   dequeue(): string | null {
-    if (this._size === 0 || !this._first) return null
+    if (this._size === 0 || !this._first) return null;
 
-    let prev = this._first
-    this._first = prev.next
-    prev.next = null
+    let prev = this._first;
+    this._first = prev.next;
+    prev.next = null;
 
-    this._size--
-    return prev.value
+    this._size--;
+    return prev.value;
   }
 }
