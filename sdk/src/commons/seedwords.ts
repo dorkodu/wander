03bf2c5c43eb50ed18070d50432a8ff414f48104
doruk eray <doruk@dorkodu.com>
2023-07-14 20:41:07 +0300

    add commons: seedwords

---
+++ sdk/src/commons/seedwords.ts
@@ -0,0 +1,26 @@
+import { bytesToHex } from "@noble/hashes/utils";
+import { wordlist } from "@scure/bip39/wordlists/english";
+import {
+  generateMnemonic,
+  mnemonicToSeedSync,
+  validateMnemonic,
+} from "@scure/bip39";
+import { HDKey } from "@scure/bip32";
+
+export function privateKeyFromSeedWords(
+  mnemonic: string,
+  passphrase?: string
+): string {
+  let root = HDKey.fromMasterSeed(mnemonicToSeedSync(mnemonic, passphrase));
+  let privateKey = root.derive(`m/44'/1237'/0'/0/0`).privateKey;
+  if (!privateKey) throw new Error("could not derive private key");
+  return bytesToHex(privateKey);
+}
+
+export function generateSeedWords(): string {
+  return generateMnemonic(wordlist);
+}
+
+export function validateWords(words: string): boolean {
+  return validateMnemonic(words, wordlist);
+}
