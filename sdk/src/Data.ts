1e6f676d55d8489eda999e91a3f2b9cd9abfde2d
doruk eray <doruk@dorkodu.com>
2023-09-30 23:47:38 +0300

    Update Data.ts

--- sdk/src/Data.ts
+++ sdk/src/Data.ts
@@ -1,30 +1,29 @@
 import { Serializable } from "child_process";
 import { CID, DID, ID, User } from "./Identity";
+import { PublicKey, UserIdentifier } from "./name/Name";
 
 export type Hash = string;
 
-export type Operation = {
-  type: string; // operation type
-  prev: string; // pointer to the CID of the previous operation in the log
-  sig: string; // base64url encoded signature of the operation
-  // ... other operation-specific data
-};
 
-export type PermissionInfo = object;
+export type PermissionInfo = Set<{
+  who: string;
+  read: boolean;
+  write: boolean;
+}>;
 
-export type FreeFormData = DID | Record<string, Attribute<any>>;
+export type FreeFormData<T = any> = DID | Document | Record<string, Attribute<T>>;
 export type EntityType = string;
 
 export type TimeStamp = number;
 
-export interface Entity {
-  id: ID;
-  type: EntityType;
+export interface DocumentTemplate {}
+
+export interface Document {
+  block: ID;
+  pathName: string;
+  owner: UserIdentifier;
   meta: Record<string, string>;
-  data: FreeFormData;
-  owner: User;
-  permissions: PermissionInfo;
-  timestamp: TimeStamp;
+  content: string;
 }
 
 export interface Attribute<T> {
@@ -33,10 +32,19 @@ export interface Attribute<T> {
   data: T;
 }
 
-export interface ImmutableEntity extends Entity {
-  parent: CID;
+export enum BlockKind {}
+
+export interface Block {
+  cid: string;
   did: DID;
+  parent?: CID;
   hash: string;
+  kind: BlockKind;
+  owner: UserIdentifier;
+  timestamp: TimeStamp;
+  permissions: PermissionInfo;
+
+  data: FreeFormData;
 }
 
 export type Method<TInput, TOutput> = {
@@ -52,45 +60,8 @@ export type StandardMethodOutput = Stringifiable;
 
 export type Schema = {
   methods: Record<string, Method<StandardMethodInput, StandardMethodOutput>>;
-  entities: Record<string, Entity>;
+  entities: Record<string, Document>;
 };
-
-/** 
-  export interface TokenLexicon {
-    lexicon: 1;
-    id: "com.example.trafficLight";
-    type: "record";
-    record: {
-      type: "object";
-      required: ["state"];
-      properties: {
-        state: { type: "string"; enum: ["red", "yellow", "green"] };
-      };
-    };
-  }
-
-  const Lexicon: LexiconDocument = {
-    lexicon: 1,
-    id: "com.example.getProfile",
-    type: "query",
-    parameters: {
-      user: { type: "string", required: true },
-    },
-    output: {
-      encoding: "application/json",
-      schema: {
-        type: "object",
-        required: ["did", "name"],
-        properties: {
-          did: { type: "string" },
-          name: { type: "string" },
-          displayName: { type: "string", maxLength: 64 },
-          description: { type: "string", maxLength: 256 },
-        },
-      },
-    },
-  };
-*/
 
 export type EventID = string;
 export type EntityID = string;
