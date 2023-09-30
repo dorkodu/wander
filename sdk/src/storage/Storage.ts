2d7115ec2f50363793faa5a491c788d4b6bd20e0
doruk eray <doruk@dorkodu.com>
2023-09-30 22:47:06 +0300

    Create Storage.ts

---
+++ sdk/src/storage/Storage.ts
@@ -0,0 +1,13 @@
+/*
+  ? A Reactive Wander Block Database Interface 
+  ! EVERYTHING IS A BLOCK 
+  * Basic CRUD operations.
+  * Add basic JSON file export
+  * Use adapter design pattern for DB options (InMemory, IndexedDB, SQLite, Static File Exports for DAG-JSON and DAG-CBOR, Postgres)
+  * Support SQLite as an adapter
+  * Support in-memory as default
+*/
+export class Storage {
+  
+  constructor() {}
+}
